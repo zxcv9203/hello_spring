1a64c6b281a58bd26efd915f493745525e5ec8e0
yongckim <zxcv9203@naver.com>
2022-03-13 23:51:26 +0900

    spring init

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,21 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "spring!!");
+        return "hello";
+    }
+
+    @GetMapping("bye")
+    public String bye(Model model) {
+        model.addAttribute("name", "golang");
+        return "bye";
+    }
+}
